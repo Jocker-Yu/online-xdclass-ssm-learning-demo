e24666a9713425a568d9066bcdd60dc3a4057d70
Jocker-Yu <741923011@qq.com>
2023-12-20 22:02:46 +0800

    1.SQL脚本1

--- online-xdclass-ssm-learning-demo/src/main/java/com/xdclass/net/onlinexdclass/controller/VideoOrderController.java
+++ online-xdclass-ssm-learning-demo/src/main/java/com/xdclass/net/onlinexdclass/controller/VideoOrderController.java
@@ -1,9 +1,12 @@
 package com.xdclass.net.onlinexdclass.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @description: VideoOrder 控制器
  * @author: Joker.yu 2023-12-20 16:19
  **/
+@RestController
 public class VideoOrderController {
 
 }
